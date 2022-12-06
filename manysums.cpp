102eee1831445b23c4675ad55b5c76d01d09f84d
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-12-06 00:12:40 -0800

    Create manysums.cpp

---
+++ manysums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {int t,a,b;
+cin>>t;
+while(t--){
+    cin>>a>>b;
+    cout<<2*(b-a)+1<<endl;
+    
+    
+}
+	// your code goes here
+	return 0;
+}
